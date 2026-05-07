458ad6535467ae364b867760bb561fc6cc5f6789
Ahmed-Nakib <office.nakib@gmail.com>
2026-05-07 14:35:40 +0600

    coomit

--- src/task4.ts
+++
@@ -1,8 +0,0 @@
-// Concepts: Type Assertion (as), unknown type
-
-
-let secretValue: unknown = "typescript is awesome";
-
-const upperValue = secretValue as string;
-
-console.log(upperValue.toUpperCase());
